5102469eeb5805c162f02f332de3ab216e2c4cb2
Nabeel Sulieman <nabsul@outlook.com>
2019-01-20 15:07:46 -0800

    Fix typo on message

--- install_kano.sh
+++ install_kano.sh
@@ -2,4 +2,4 @@
 echo Downloading SulieChat
 curl -sL https://github.com/nabsul/suliechat/releases/download/v0.9/suliechat_arm -o /usr/local/bin/suliechat
 chmod 755 /usr/local/bin/suliechat
-echo SulieChat successfully isntalled. Have fun!
+echo SulieChat successfully installed. Have fun!
