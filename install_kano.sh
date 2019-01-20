23daf672e4059bff72186d1afd527d207eec1817
Nabeel Sulieman <nabsul@outlook.com>
2019-01-19 18:52:41 -0800

    Update the install script to the release file path

--- install_kano.sh
+++ install_kano.sh
@@ -1,5 +1,5 @@
 #!bash
 echo Downloading SulieChat
-curl https://github.com -o /usr/local/bin/suliechat
+curl https://github.com/nabsul/suliechat/releases/download/v0.9/suliechat_arm -o /usr/local/bin/suliechat
 chmod 755 /usr/local/bin/suliechat
 echo SulieChat successfully isntalled. Have fun!
